896aa24de9f9ae4b707530579022a87741c68358
Kithrian <58808821+codefuturist@users.noreply.github.com>
2024-10-14 14:05:41 +0200

    -

--- publish.sh
+++ publish.sh
@@ -1,13 +1,23 @@
+git checkout master
+
+
+helm package homarr
+
+helm package application
+
+
+helm repo index . --url https://codefuturist.github.io/application/
+
 # Step 1: Checkout to the destination branch
 git checkout gh-pages
 
 # Step 2: Copy the files from the 'main' branch to 'gh-pages'
-git checkout main -- index.yaml
-git checkout main -- homarr-5.1.0.tgz
-git checkout main -- application-5.1.0.tgz
+git checkout master -- index.yaml
+git checkout master -- homarr-5.1.0.tgz
+git checkout master -- application-5.1.0.tgz
 
 # Step 3: Stage the copied files
-git add index.yaml my-chart-0.1.0.tgz
+# git add index.yaml application-5.1.0.tgz homarr-5.1.0.tgz
 
 # Step 4: Commit the changes
 git commit -m "Move index.yaml and Helm chart from main to gh-pages"
